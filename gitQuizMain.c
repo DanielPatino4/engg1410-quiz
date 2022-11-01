fe650e13233f575fd60edf6f804a71da192909c5
DanielPatino4 <116667951+DanielPatino4@users.noreply.github.com>
2022-11-01 13:42:42 -0400

    Update gitQuizMain.c
    Added an else statement if doesnt click x

--- gitQuizMain.c
+++ gitQuizMain.c
@@ -9,8 +9,8 @@ int main(void)
 
     if(val == 'y'){
         printf("You know how to use Git!");
-    }else if(val == 'x'){
-        printf("You really know what you are doing");
+    }else{
+        printf("You dont really know what you are doing");
     }
 
     printf("\n");
